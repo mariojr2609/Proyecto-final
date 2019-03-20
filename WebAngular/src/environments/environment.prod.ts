aeddf518e63a305cd16d0c2b8817de970bd131c4
mariojr2609 <mariojr2609@gmail.com>
2019-03-21 00:29:43 +0100

    t

--- WebAngular/src/environments/environment.prod.ts
+++ WebAngular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-proyecto-final.herokuapp.com',
+  apiUrl: 'https://api-proyecto-final.herokuapp.com/',
   masterkey: 'Ubu5O0SQZJbSlgKEkyUHVIL4pQ7w4OTe'
 };
--- WebAngular/src/environments/environment.ts
+++ WebAngular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false, 
-  apiUrl: 'https://api-proyecto-final.herokuapp.com',
+  apiUrl: 'https://api-proyecto-final.herokuapp.com/',
   masterkey: 'Ubu5O0SQZJbSlgKEkyUHVIL4pQ7w4OTe'
 };
 
